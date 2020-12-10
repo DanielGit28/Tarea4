a412c0c3a78421e41ebc15fc4f15edb64058d141
Daniel <dzunigar@ucenfotec.ac.cr>
2020-12-09 22:01:34 -0400

    Base de datos generada

---
+++ BaseDatos/bdtarea4_movimiento.sql
@@ -0,0 +1,57 @@
+CREATE DATABASE  IF NOT EXISTS `bdtarea4` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `bdtarea4`;
+-- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: bdtarea4
+-- ------------------------------------------------------
+-- Server version	8.0.22
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `movimiento`
+--
+
+DROP TABLE IF EXISTS `movimiento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `movimiento` (
+  `idmovimiento` int NOT NULL AUTO_INCREMENT,
+  `fecha` date NOT NULL,
+  `descripcion` varchar(100) NOT NULL,
+  `monto` double NOT NULL,
+  `tipoMovimiento` varchar(12) NOT NULL,
+  `numeroCuenta` int NOT NULL,
+  PRIMARY KEY (`idmovimiento`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `movimiento`
+--
+
+LOCK TABLES `movimiento` WRITE;
+/*!40000 ALTER TABLE `movimiento` DISABLE KEYS */;
+INSERT INTO `movimiento` VALUES (1,'2020-12-09','Primero',10000,'DEPOSITO',1235),(2,'2020-12-09','Primer ahorro',15000,'DEPOSITO',1234),(3,'2020-12-09','Primer deposito',40000,'DEPOSITO',1456),(4,'2020-12-09','Primer retiro',10000,'RETIRO',1456),(5,'2020-12-09','X',10000,'DEPOSITO',11811),(6,'2020-12-09','p',5000,'RETIRO',11811),(7,'2020-12-09','ou',10000,'DEPOSITO',1234),(8,'2020-12-09','Pi',10000,'RETIRO',1234),(9,'2020-12-09','Pi',10000,'DEPOSITO',1234),(10,'2020-12-09','Retiro 1',2000,'RETIRO',1234),(11,'2020-12-09','Grande',100000,'DEPOSITO',1234),(12,'2020-12-09','retiro grande',55000,'RETIRO',1234),(13,'2020-12-09','po',5000,'RETIRO',1234),(14,'2020-12-09','ahorro programdo',10000,'DEPOSITO',1456),(15,'2020-12-09','qer',1000,'RETIRO',1456);
+/*!40000 ALTER TABLE `movimiento` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-12-09 22:00:48
